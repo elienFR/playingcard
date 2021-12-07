32ba2d671ed2bbc3e33c81da2bacef0c4dd33af5
Elien <elienferre@yahoo.fr>
2021-12-07 17:34:25 +0100

    adding controller features

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -2,32 +2,29 @@ package controller;
 
 import model.Deck;
 import model.Player;
+import model.PlayingCard;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class View {
-  public void something(){
+  public void something() {
 
-  };
+  }
 
-  public void setController(GameController gc){
+  public void setController(GameController gc) {
 
-  };
+  }
 }
 
 public class GameController {
-  enum GameState {
-    AddingPlayer, CardDealt, WinnerRevealed;
-  }
   Deck deck;
   List<Player> players;
   Player winner;
   View view;
-
   GameState gameState;
 
-  public GameController(Deck deck, View view){
+  public GameController(Deck deck, View view) {
     super();
     this.deck = deck;
     this.view = view;
@@ -36,13 +33,13 @@ public class GameController {
     view.setController(this);
   }
 
-  public void run(){
+  public void run() {
     while (gameState == GameState.AddingPlayer) {
       view.something();
     }
 
     switch (gameState) {
-      case CardDealt:
+      case CardsDealt:
         view.something();
         break;
       case WinnerRevealed:
@@ -50,4 +47,85 @@ public class GameController {
         break;
     }
   }
+
+  public void addPlayer(String playerName) {
+    if (gameState == GameState.AddingPlayer) {
+      players.add(new Player(playerName));
+      view.something();
+    }
+  }
+
+  public void startGame() {
+    if (gameState != GameState.CardsDealt) {
+      deck.shuffle();
+      for (Player player : players) {
+        player.addCardToHand(deck.removeTopCard());
+        view.something();
+      }
+      gameState = GameState.CardsDealt;
+    }
+    this.run();
+  }
+
+  public void fliCards() {
+    for (Player player : players) {
+      PlayingCard pc = player.getCard(0);
+      pc.flip();
+      view.something();
+    }
+
+    evaluateWinner();
+    displayWinner();
+    rebuildDeck();
+    gameState = GameState.WinnerRevealed;
+    this.run();
+  }
+
+  public void evaluateWinner() {
+    Player bestPlayer = null;
+    int bestRank = -1;
+    int bestSuit = -1;
+
+    for (Player player : players){
+      boolean newBestPlayer = false;
+
+      if(bestPlayer == null) {
+        newBestPlayer = true;
+      } else {
+        PlayingCard pc = player.getCard(0);
+        int thisRank = pc.getRank().value();
+        if(thisRank >= bestRank){
+          if(thisRank > bestRank){
+            newBestPlayer = true;
+          } else {
+            if (pc.getSuit().value() > bestSuit){
+              newBestPlayer = true;
+            }
+          }
+        }
+      }
+      if (newBestPlayer) {
+        bestPlayer = player;
+        PlayingCard pc = player.getCard(0);
+        bestRank = pc.getRank().value();
+        bestSuit = pc.getSuit().value();
+      }
+    }
+  }
+
+  public void displayWinner() {
+    view.something();
+  }
+
+  public void rebuildDeck() {
+    for (Player player : players) {
+      deck.returnCardToDeck(player.removeCard());
+    }
+  }
+
+  enum GameState {
+    AddingPlayer, CardsDealt, WinnerRevealed;
+  }
+
+
 }
